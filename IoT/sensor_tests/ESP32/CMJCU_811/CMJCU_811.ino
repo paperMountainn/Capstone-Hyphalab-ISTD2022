04051a2fdcb0500cea8f975b118dde6887c22545
Zishan Pan <zishan_pan@mymail.sutd.edu.sg>
2022-05-23 20:04:18 +0800

    add co2 sensor test

---
+++ IoT/sensor_tests/ESP32/CMJCU_811/CMJCU_811.ino
@@ -0,0 +1,57 @@
+/***************************************************************************
+  This is a library for the CCS811 air
+
+  This sketch reads the sensor
+
+  Designed specifically to work with the Adafruit CCS811 breakout
+  ----> http://www.adafruit.com/products/3566
+
+  These sensors use I2C to communicate. The device's I2C address is 0x5A
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit andopen-source hardware by purchasing products
+  from Adafruit!
+
+  Written by Dean Miller for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ***************************************************************************/
+
+// https://learn.adafruit.com/adafruit-ccs811-air-quality-sensor?view=all
+
+// sensor works well. Prints data like this
+// 20:02:59.596 -> CO2: 403ppm, TVOC: 0
+
+
+#include "Adafruit_CCS811.h"
+
+Adafruit_CCS811 ccs;
+
+void setup() {
+  Serial.begin(115200);
+
+  Serial.println("CCS811 test");
+
+  if(!ccs.begin()){
+    Serial.println("Failed to start sensor! Please check your wiring.");
+    while(1);
+  }
+
+  // Wait for the sensor to be ready
+  while(!ccs.available());
+}
+
+void loop() {
+  if(ccs.available()){
+    if(!ccs.readData()){
+      Serial.print("CO2: ");
+      Serial.print(ccs.geteCO2());
+      Serial.print("ppm, TVOC: ");
+      Serial.println(ccs.getTVOC());
+    }
+    else{
+      Serial.println("ERROR!");
+      while(1);
+    }
+  }
+  delay(500);
+}
