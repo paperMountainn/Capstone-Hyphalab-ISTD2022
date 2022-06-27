bac18aea09edec972198193981ef8138cdb2e341
SS0706 <stephanieseow11@gmail.com>
2022-06-28 01:25:55 +0800

    Update Card.jsx

--- IoT/iot-webapp/FrontEnd/src/components/card/Card.jsx
+++ IoT/iot-webapp/FrontEnd/src/components/card/Card.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
-import { Card } from 'semantic-ui-react'
+import React from 'react';
+import { Card } from 'semantic-ui-react';
 import './card.scss';
 import { RiTempHotLine } from "react-icons/ri";
+// import {Grid, Row, Col} from "react-bootstrap";
+import Grid from '@mui/material/Button';
+
+import "./card.css";
+
 export const MyCard = ({header, meta, icon, color}) => {
   return (
         <Card className='cardClass' color={color}>
           <Card.Content>
 
-            <Card.Header >{header}</Card.Header>
-            <Card.Meta>{meta}</Card.Meta>
+          <Grid container spacing={1}> 
+          
+          <Grid item>
             <Card.Description>
               <h1>{icon}</h1>
             </Card.Description>
+          </Grid>
+          <Grid item>
+            <Card.Header >{header}</Card.Header>
+          </Grid>
+          <Grid item>
+          <Card.Meta>{meta}</Card.Meta>
+          </Grid>
+
+          </Grid>
+
           </Card.Content>
         </Card>
   )
